0036fbd5deb6e877a6a9341e05e89a883b304c03
nickm <nickmangarella89@gmail.com>
2021-02-25 18:41:00 -0500

    created function for hbar

--- static/js/app.js
+++ static/js/app.js
@@ -1,4 +1,8 @@
-d3.json("data/data.json").then((importedData) => {
-    console.log(importedData);
-    var data = importedData;
+// Read the JSON file
+d3.json("data/samples.json").then((data) => {
+
+    // Grab values from the json object to build the plots
+    var sampleValues = data.samples.sample_values
+    var otuIds = data.samples.otu_ids
+    var otuLabels = data.samples.otu_labels
 });
