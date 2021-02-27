51a05ed1459bfc1692de62e00cd32a69ea5c96c7
nickm <nickmangarella89@gmail.com>
2021-02-26 21:50:29 -0500

    setup function for plots

--- static/js/app.js
+++ static/js/app.js
@@ -1,8 +1,24 @@
-// Read the JSON file
-d3.json("data/samples.json").then((data) => {
+function unpack(rows, index) {
+    return rows.map(function(row) {
+      return row[index];
+    });
+  }
 
-    // Grab values from the json object to build the plots
-    var sampleValues = data.samples.sample_values
-    var otuIds = data.samples.otu_ids
-    var otuLabels = data.samples.otu_labels
-});
+function buildHbar() {
+    // Read the JSON file
+    d3.json("data/samples.json").then((data) => {
+
+        // Grab values from the json object to build the plots
+        var sampleValues = unpack(data.samples.sample_values);
+        var otuIds = unpack(data.samples.otu_ids);
+        var otuLabels = unpack(data.samples.otu_labels);
+
+        console.log(sampleValues);
+        console.log(otuIds);
+        console.log(otuLabels);
+
+
+    });
+}
+
+buildHbar();
