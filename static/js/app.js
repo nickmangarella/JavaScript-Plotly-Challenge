0f74fb0d67d2390442cf4f55349f046e7d45a7c1
nickm <nickmangarella89@gmail.com>
2021-02-28 20:42:50 -0500

    chose simplified script file

--- static/js/app.js
+++ static/js/app.js
@@ -1,50 +1,60 @@
-// Initial function that renders the names to the dropdown
+// Function to initialize the plots
 function init() {
     d3.json("samples.json").then((importedData) => {
-        createOptions(importedData.names, "selDataset");
+
+        // For each sample name append them to the "dropdownMenu"
+        importedData.names.forEach((name) => {
+            
+            var dropdownMenu = d3.select("#selDataset").property("value", name);
+            
+            dropdownMenu.append("option").text(name);
+        });
+
+        // Default to the first samples plots when visiting the webpage
+        var name1 = importedData.names[0];
+        buildPlots(name1);
     });
 }
 
-function createOptions(data, cb_element) {
-    element = document.getElementById(cb_element);
-    for (var i = 0; i < data.length; i++) {
-      var option = document.createElement('option');
-      option.text = data[i];
-      element.appendChild(option);
-    }
-}
+// Function to build all plots
+function buildPlots() {
+    d3.json("samples.json").then((importedData) => {
 
-// Function to build the plots
-function buildPlots(name) {
-    var dropdownMenu = d3.select("#selDataset");
-    
-    // Assign the value of the dropdown menu option to a variable
-    var dataset = dropdownMenu.property("value");
-    console.log(dataset);
-    
-    // Read the JSON file
-    d3.json("samples.json").then((importedData) => {        
+        // Simplify the samples object to a variable
+        var samples = importedData.samples;
+
+        // Sort the data by sample values
+        var sortedData = samples.sort((a, b) => parseFloat(b.sample_values) - parseFloat(a.sample_values));
+
+        // Slice the first 10 objects for plotting
+        var slicedData = sortedData.slice(0, 10).reverse();
         
-        // Grab values from the json object to build the plots
-        var sampleValues = importedData.samples.map(x => x.sample_values);
-        var otuIds = importedData.samples.map(x => x.otu_ids);
-        var otuLabels = importedData.samples.map(x => x.otu_labels);
+        // Create horizontal bar chart
+        var hbar = [{
+            x: slicedData.map(x => x.sample_values),
+            y: slicedData.map(x => x.otu_ids),
+            text: slicedData.map(x => x.otu_labels),
+            type: "bar",
+            orientation: "h"
+        }];
 
-        console.log(sampleValues);
-        console.log(otuIds);
-        console.log(otuLabels);
-
-
-
+        // Layout for horizontal bar chart
+        var hbarLayout = {
+            group: "barmode"
+        };
+        
+        // Render the plot to the div tag with id "bar"
+        Plotly.newPlot("bar", hbar, hbarLayout);
     });
 }
 
+// Function to call the buildPlots function in the HTML
 function optionChanged(sample) {
     buildPlots(sample);
 }
-// Call the inital function
+
+// Initialize the webpage plots
 init();
 
 // Event to render the corresponding plots to the selected name from the dropdown
 d3.selectAll("#selDataset").on("change", buildPlots);
-
