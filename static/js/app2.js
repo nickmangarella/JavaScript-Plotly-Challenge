8205a9046ebd4db81bd8e03ac90a5ca54906a6a0
nickm <nickmangarella89@gmail.com>
2021-02-28 20:40:24 -0500

    commented script

--- static/js/app2.js
+++ static/js/app2.js
@@ -1,56 +1,59 @@
+// Function to initialize the plots
 function init() {
     d3.json("samples.json").then((importedData) => {
 
+        // For each sample name append them to the "dropdownMenu"
         importedData.names.forEach((name) => {
             
             var dropdownMenu = d3.select("#selDataset").property("value", name);
             
             dropdownMenu.append("option").text(name);
         });
+
+        // Default to the first samples plots when visiting the webpage
         var name1 = importedData.names[0];
         buildPlots(name1);
     });
 }
 
+// Function to build all plots
 function buildPlots() {
     d3.json("samples.json").then((importedData) => {
 
-        var samplesData = [];
+        // Simplify the samples object to a variable
+        var samples = importedData.samples;
 
-        importedData.samples.forEach((sample) => {
-            samplesData.push({
-                "sample_values": sample.sample_values,
-                "otu_ids": sample.otu_ids,
-                "otu_labels": sample.otu_labels
-            });
-        });
+        // Sort the data by sample values
+        var sortedData = samples.sort((a, b) => parseFloat(b.sample_values) - parseFloat(a.sample_values));
 
-        var sortedData = samplesData.sort((a, b) => parseFloat(b.sample_values) - parseFloat(a.sample_values));
-        console.log(sortedData);
+        // Slice the first 10 objects for plotting
+        var slicedData = sortedData.slice(0, 10).reverse();
         
-        console.log(sortedData.map(x => x.sample_values).slice(0, 10));
-
+        // Create horizontal bar chart
         var hbar = [{
-            x: sortedData.map(x => x.sample_values[0]).slice(0, 10),
-            y: sortedData.map(x => x.otu_ids[0]).slice(0,10),
-            text: sortedData.map(x => x.otu_labels[0]).slice(0, 10),
+            x: slicedData.map(x => x.sample_values),
+            y: slicedData.map(x => x.otu_ids),
+            text: slicedData.map(x => x.otu_labels),
             type: "bar",
             orientation: "h"
         }];
 
+        // Layout for horizontal bar chart
         var hbarLayout = {
             group: "barmode"
         };
-
+        
+        // Render the plot to the div tag with id "bar"
         Plotly.newPlot("bar", hbar, hbarLayout);
     });
 }
 
+// Function to call the buildPlots function in the HTML
 function optionChanged(sample) {
     buildPlots(sample);
 }
 
-// Initialize the webpage features
+// Initialize the webpage plots
 init();
 
 // Event to render the corresponding plots to the selected name from the dropdown
